b3384ce106af845f7336b542f310e167e1a75348
Krisztian Stancz <krisztian.stancz@gmail.com>
2017-05-24 12:42:09 +0200

    add solutions

--- week-04/day-2/08_circular_buffer/main.c
+++ week-04/day-2/08_circular_buffer/main.c
@@ -24,7 +24,7 @@ struct circular_buffer {
 // not just the array inside)
 // It should take a normal array and it's length as argument, and it should fill all the buffer values from the array
 
-struct *circular_buffer new_buffer(int *array, int size)
+struct circular_buffer *new_buffer(int *array, int size)
 {
     // Allocate memory to new buffer
     int *buffer = (int*) calloc(size, sizeof(int));
@@ -32,9 +32,9 @@ struct *circular_buffer new_buffer(int *array, int size)
     // Allocate memory to the structure
     struct circular_buffer *new_buffer = (struct circular_buffer*) malloc(sizeof(struct circular_buffer));
 
-    new_buffer.head_p = &buffer[0];
-    new_buffer.tail_p = &buffer[size - 1];
-    new_buffer.buffer_length = size;
+    new_buffer->head_p = &buffer[0];
+    new_buffer->tail_p = &buffer[size - 1];
+    new_buffer->buffer_length = size;
 
     // Fill in new buffer with data
     for (int i = 0; i < size; i++) {
@@ -42,20 +42,77 @@ struct *circular_buffer new_buffer(int *array, int size)
     }
 
     // Set read and write pointers
-    new_buffer.read_p = new_buffer.head_p;
-    new_buffer.write_p = new_buffer.head_p;
+    new_buffer->read_p = new_buffer->head_p;
+    new_buffer->write_p = new_buffer->head_p;
 
     // Return pointer to the structure
     return new_buffer;
-};
+}
 
 // TODO:
 // Create a function that resizes a CircularBuffer, it should take a CircularBuffer
 // as a pointer and change that buffers length and reallocate it's array as well. (copying it's values)
+int resize_buffer(struct circular_buffer *buffer, int new_size)
+{
+    int return_value = 0;
+
+    // Need to remember where the read an write pointers are
+    int read_pos = buffer->read_p - buffer->head_p;
+    int write_pos = buffer->write_p - buffer->head_p;
+
+    // Resize buffer
+    int *resized_buffer = (int*) realloc(buffer->head_p, new_size * sizeof(int));
+    buffer->buffer_length = new_size;
+
+    // Setup the pointers
+    buffer->head_p = &resized_buffer[0];
+    buffer->tail_p = &resized_buffer[new_size-1];
+    buffer->read_p = buffer->head_p + read_pos;
+    buffer->write_p = buffer->head_p + write_pos;
+
+    // If we decrease the size the read/write pointers may fall out of range
+    // In that case reset them to the tail position and let the user know
+    if (buffer->read_p > buffer->tail_p) {
+        buffer->read_p = buffer->tail_p;
+        return_value += 1;
+    }
+
+    if (buffer->write_p > buffer->tail_p) {
+        buffer->write_p = buffer->tail_p;
+        return_value += 2;
+    }
+
+    return return_value;
+}
 
 // TODO:
 // Create a function that takes a CircularBuffer and returns a new CircularBuffer that only contains
 // the even numbers from the old CircularBuffer
+int create_even_buffer(struct circular_buffer *buffer)
+{
+    // Save even numbers in incoming buffer into a temporary array,
+    // which has the same size as the buffer
+    int *array = (int*) calloc(buffer->buffer_length, sizeof(int));
+    int i = 0;
+    int *j;
+    for (j = buffer->head_p; j <= buffer->tail_p; j++) {
+        if (*j % 2 == 0) {
+            array[i] = *j;
+            i++;    // i will return size of array too
+        }
+    }
+
+    // Cut away the empty space from array
+    int *shortend_array = (int*) realloc(array, i * sizeof(int));
+
+    // Create new circular buffer with the even numbers
+    struct circular_buffer *even_buffer = new_buffer(shortend_array, i);
+
+    // Cleanup garbage
+    free(shortend_array);
+
+    return even_buffer;
+}
 
 // TODO:
 // Try all the functions in the main function, please deallocate any buffer and the array inside it after using them
@@ -64,6 +121,48 @@ int main()
 {
     int array[] = {1, 2, 3, 4, 5, 6, 7, 8};
 
-    size = sizeof(array) / sizeof(array[0]);
+    int size = sizeof(array) / sizeof(array[0]);
+
+    struct circular_buffer *c_buffer = new_buffer(array, size);
+
+    printf("buffer->head_p: %d\n", c_buffer->head_p);
+    printf("buffer->tail_p: %d\n", c_buffer->tail_p);
+    printf("buffer->read_p: %d\n", c_buffer->read_p);
+    printf("buffer->write_p: %d\n", c_buffer->write_p);
+    printf("buffer->buffer_length: %d\n", c_buffer->buffer_length);
+
+    for (int i = 0; i < c_buffer->buffer_length; i++) {
+        printf("%d\t%d\n", i, *(c_buffer->head_p + i));
+    //    printf("%d\t%d\n", i, c_buffer->head_p[i]);
+    }
+
+    resize_buffer(c_buffer, 7);
+
+    printf("buffer->head_p: %d\n", c_buffer->head_p);
+    printf("buffer->tail_p: %d\n", c_buffer->tail_p);
+    printf("buffer->read_p: %d\n", c_buffer->read_p);
+    printf("buffer->write_p: %d\n", c_buffer->write_p);
+    printf("buffer->buffer_length: %d\n", c_buffer->buffer_length);
+
+    for (int i = 0; i < c_buffer->buffer_length; i++) {
+        printf("%d\t%d\n", i, *(c_buffer->head_p + i));
+    //    printf("%d\t%d\n", i, c_buffer->head_p[i]);
+    }
+
+    struct circular_buffer *e_buffer = create_even_buffer(c_buffer);
+
+    printf("buffer->head_p: %d\n", e_buffer->head_p);
+    printf("buffer->tail_p: %d\n", e_buffer->tail_p);
+    printf("buffer->read_p: %d\n", e_buffer->read_p);
+    printf("buffer->write_p: %d\n", e_buffer->write_p);
+    printf("buffer->buffer_length: %d\n", e_buffer->buffer_length);
+
+    for (int i = 0; i < e_buffer->buffer_length; i++) {
+        printf("%d\t%d\n", i, *(e_buffer->head_p + i));
+    //    printf("%d\t%d\n", i, c_buffer->head_p[i]);
+    }
+
+
+
     return 0;
 }
---
+++ week-04/day-2/09_student_subject_grade/main.c
@@ -0,0 +1,105 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum {MATH, HISTORY, ENGLISH, SCIENCE} subject_t;
+
+typedef struct {
+    subject_t subject;
+    int value;
+} grade_t;
+
+typedef struct {
+    char name[128];
+    int age;
+    grade_t* grades;
+    int grade_count;
+} student_t;
+
+// TODO:
+// Create a function the constructs a Student (returns a pointer to a new student)
+// It should take it's name and it's age as parameter
+// It should allocate 5 grades
+// It should set the grade_count to 0
+
+student_t *new_student(const char *s_name, int s_age)
+{
+    // Create new student structure and grade array
+    student_t *new_student = (student_t*) malloc(sizeof(student_t));
+
+    // Setup structure values
+    new_student->grades = (grade_t*) calloc(5, sizeof(grade_t));
+    new_student->age = s_age;
+    new_student->grade_count = 0;
+
+    // Copy student name
+    for (int i = 0; i < 128; i++) {
+        new_student->name[i] = s_name[i];
+    }
+
+    return new_student;
+}
+
+// TODO:
+// Create a function that takes 3 parameters:
+// A pointer to a Student
+// A Subject
+// An int (1-5)
+// It should set the last grade's values with the given arguments
+void set_grade(student_t *student, subject_t subject, int grade)
+{
+    student->grades[student->grade_count].subject = subject;
+    student->grades[student->grade_count].value = grade;
+    student->grade_count++;
+
+    // If we have more than 5 grades we turn it into a circular buffer
+    if (student->grade_count > 4)
+        student->grade_count = 0;
+
+    return;
+}
+
+// TODO:
+// Create a function that takes a Student as a pointer and returns the subject
+// that the student has worst grade in
+subject_t worst_subject(student_t *student)
+{
+    int min_pos = 0;
+    int min_grade = 5;
+    for (int i = 0; i < student->grade_count - 1; i++) {
+        if (student->grades[i].value < min_grade) {
+            min_grade = student->grades[i].value;
+            min_pos = i;
+        }
+    }
+    return student->grades[min_pos].subject;
+}
+
+// TODO:
+// Create a function that deconstructs a Student
+// It should take a pointer that points to the student
+// It should free each Grade and the Student itself
+void delete_student(student_t *student)
+{
+    free(student->grades);
+    free(student);
+
+    return;
+}
+
+int main()
+{
+    student_t *paul = new_student("Paul Smith", 16);
+    set_grade(paul, MATH, 3);
+    set_grade(paul, HISTORY, 1);
+    set_grade(paul, ENGLISH, 3);
+    set_grade(paul, SCIENCE, 2);
+
+    char subjects[][10] = {"MATH", "HISTORY", "ENGLISH", "SCIENCE"};
+
+    printf("Worst subject of %s: %s", paul->name, subjects[worst_subject(paul)]);
+
+    delete_student(paul);
+
+    return 0;
+}
